c272abab46225da2e2678a6fce617e0de197befe
WOoOinux <contact@woinux.fr>
2020-01-13 17:50:56 +0100

    Update compilation rules

--- gulpfile.js
+++ gulpfile.js
@@ -17,12 +17,12 @@ var dest = config.dest; // Compile folder
 
 // Tasks
 gulp.task('css', function () {
-  return gulp.src(src + '/**/*.scss')
+  return gulp.src(src + '/main.scss')
     .pipe(plugins.sassGlob()) // Allow glob import for SASS
     .pipe(plugins.sass()) // Compile CSS
     .pipe(plugins.csscomb()) // Re-ordonate properties
     .pipe(plugins.cssbeautify({indent: '    '})) // Re-indent
-    .pipe(plugins.autoprefixer()) // Auto-prefix CSS3 properties
+    .pipe(plugins.autoprefixer()) // Auto-prefidx CSS3 properties
     .pipe(gulp.dest(dest + '/'))
     .pipe(browserSync.stream()); // Sync browser
 });
@@ -36,15 +36,15 @@ gulp.task('minify-css', function () {
 });
 
 gulp.task('js', function() {
-    return gulp.src(src + '/assets/**/*.js')
+    return gulp.src(src + '/**/*.js')
     .pipe(plugins.jshint()) // Check JS files for convention rules
     .pipe(plugins.jshint.reporter('default')) // Report errors
-    .pipe(plugins.babel()) // Compile JS for old browsers
-    .pipe(gulp.dest(dest + '/assets/'));
+    .pipe(plugins.babel()); // Compile JS for old browsers
+    // .pipe(gulp.dest(dest + '/'));
 });
 
 gulp.task('minify-js', function() {
-    return gulp.src(src + '/assets/**/*.js')
+    return gulp.src(src + '/**/*.js')
     .pipe(uglify()) // Minify JS
     .pipe(plugins.concat('scripts.min.js')) // Concatenate all JS file in one
     .pipe(gulp.dest(dest + '/'));
